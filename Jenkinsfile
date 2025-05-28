b526b725b145f38e5a3a3e3ba0c8aa45f0161491
Rafalimanana Sébastien <sebastienrafalimanana@gmail.com>
2025-05-28 13:56:42 +0300

    chore:fix feautre

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,7 +1,7 @@
 pipeline {
   agent {
     kubernetes {
-      label 'jenkins-agent-my-app'
+      label 'jenkins-agent-my-app-feature'
       yaml """
 apiVersion: v1
 kind: Pod
--- app.py
+++ app.py
@@ -12,5 +12,9 @@ def hello_world():
 def hello_user(username):
     return f'Hello {username}!\n'
 
+@app.route('/feature/<string:username>')
+def feature_user(username):
+    return f'this is a future for {username}!\n'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
